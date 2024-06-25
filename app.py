00473c6a7d60353e1756e836ccea369b10659026
Diallo75012 <11600022@students.koi.edu.au>
2024-06-25 14:41:38 +0200

    create the new states to handle last output message saved to states to be used by agents later on. have added a popup message to notify user to post the tweet or if there is an error. have checked the states and it all works fine. have added the functionality to tweet. now need to add agents llms to handle the chat messages and the tweet task as well. we will use two different agent teams sharing the same tools.

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import markdownify
 from dotenv import load_dotenv
 # Pydantic stype function  argument type specifications
 from typing import List, Dict, Union, Optional, Callable
+# tweeter library
+import tweepy
 
 # env vars
 load_dotenv(dotenv_path='.env')
@@ -26,33 +28,98 @@ class FileState:
   contents: str
   pdf_file_path: str
 
-result = {
-  "state_update_status": False,
-  "state_updated": "",
-  "state_update_message": "",
-  "last_modified": "",
-  "notify_user": "",
-  "error": ""
-}
-
 @me.stateclass
 class FormState:
-    consumer_key: bool = False
-    consumer_secret: bool = False
-    access_token: bool = False
-    access_token_secret: bool = False
-    is_submitted: bool = False
-    last_modified: str = ""
-    result: Dict[str, str|bool]
+  consumer_key: bool = False
+  consumer_secret: bool = False
+  access_token: bool = False
+  access_token_secret: bool = False
+  is_submitted: bool = False
+  last_modified: str = ""
+  result: Dict[str, str|bool]
 
 
 @me.stateclass
 class PageState:
-    sidenav_open: bool = True
+  sidenav_open: bool = True
+
+@me.stateclass
+class TweetState:
+  tweet_message: str = ""
+  tweet_posted = Dict[str, str]
+
+@me.stateclass
+class PopupState:
+  popup_visible: bool = False
+  popup_message: str = ""
+  popup_style: Dict[str, str]
 
 # Env Vars to set
 path = "./docs/World_Largest_Floods_paper.pdf"
 SIDENAV_WIDTH = 350
+
+# Function to show popup message
+def show_popup(state, message, message_type):
+  styles = {
+    "error": {"color": "red"},
+    "validated": {"color": "green"},
+    "info": {"color": "blue"}
+  }
+  state.popup_message = message
+  state.popup_style = styles.get(message_type, {"color": "black"})
+  state.popup_visible = True
+  print(f"Popup State Updated: {state}")
+
+def close_popup(state, e: me.ClickEvent):
+  state.popup_visible = False
+  state.popup_message = ""
+  state.popup_style = {}
+  print(f"Pop up closed: All state fields reset -> {state}")
+
+# post tweet
+# Function to post a tweet using saved tokens
+# add here llm agent call to work on the tweet
+def post_tweet(e: me.ClickEvent):
+    tweet_state = me.state(TweetState)
+    popup_state = me.state(PopupState)
+    auth = tweepy.OAuth1UserHandler(
+        os.getenv("CONSUMER_KEY"),
+        os.getenv("CONSUMER_SECRET"),
+        os.getenv("ACCESS_TOKEN"),
+        os.getenv("ACCESS_TOKEN_SECRET")
+    )
+    api = tweepy.API(auth)
+    try:
+      response = api.update_status(tweet_state.tweet_message) # here change the message with the llm agent response and pass the state message to the llms
+      print("Tweet posted and tokens cleared.")
+      tweet_state.tweet_posted = {
+        "tweet": response.text,
+        "create_at": response.created_at,
+        "user_name": response.user.name,
+      }
+      message_type = "validated"
+      # delete env vars
+      clear_tokens()
+      # show the popup message
+      show_popup(popup_state, tweet_state.tweet_posted, message_type)
+
+    except Exception as e:
+      tweet_state.tweet_posted = {
+        "error": f"An error accured when posting tweet: {e}"
+      }
+      message_type = "error"
+      # show the popup message
+      print("in post tweet exception error part")
+      print("last chat message", tweet_state.tweet_message)
+      show_popup(popup_state, tweet_state.tweet_posted, message_type)
+
+# Function to clear environment variables
+def clear_tokens():
+    os.environ["CONSUMER_KEY"] = ""
+    os.environ["CONSUMER_SECRET"] = ""
+    os.environ["ACCESS_TOKEN"] = ""
+    os.environ["ACCESS_TOKEN_SECRET"] = ""
+    print("Tokens env. vars cleared!")
 
 # function to create dynamic env vars
 def create_dynamic_env(env_path: str, env_vars: Dict[str, str]):
@@ -68,14 +135,7 @@ def create_dynamic_env(env_path: str, env_vars: Dict[str, str]):
             env_file.write(f"{key}={value}\n")
     load_dotenv(dotenv_path='.dynamic.env', override=True)
     print("Dynamic Env vars created and accessible using: os.getenv('variable_name')")
- 
-# Function to clear environment variables
-def clear_tokens():
-    os.environ['CONSUMER_KEY'] = ""
-    os.environ['CONSUMER_SECRET'] = ""
-    os.environ['ACCESS_TOKEN'] = ""
-    os.environ['ACCESS_TOKEN_SECRET'] = ""
-    print("Tokens env. vars cleared!")
+
 
 # CLICK EVENT MANAGEMENT
 def on_click(e: me.ClickEvent):
@@ -89,6 +149,15 @@ env_dict = {
     "CONSUMER_SECRET": "",
     "ACCESS_TOKEN": "",
     "ACCESS_TOKEN_SECRET": ""
+}
+
+result = {
+  "state_update_status": False,
+  "state_updated": "",
+  "state_update_message": "",
+  "last_modified": "",
+  "notify_user": "",
+  "error": ""
 }
 
 # update state form bool values to 'True'
@@ -136,6 +205,7 @@ def handle_message(msg):
 def submit_form(e: me.ClickEvent):
   state = me.state(FormState)
   form_state = me.state(FormState)
+  popup_state = me.state(PopupState)
   print("Submit form states: ", state)
    
   # update state form boolean values to 'True'
@@ -170,11 +240,13 @@ def submit_form(e: me.ClickEvent):
 
     # tell user to press the post to tweeter button
     result["notify_user"] = "Post Tweet Now!"
+    show_popup(popup_state, result["notify_user"], "info")
 
   else:
     # warn user that all fields are required
     result["error"] = "Error: All fields are required."
-
+    print("JUNNNNNNKOOOOO!")
+    show_popup(popup_state, "All Fields Are Required!", "error")
   form_state.result = result
   print("State Result: ", form_state.result)
 
@@ -231,21 +303,24 @@ def handle_upload(event: me.UploadEvent):
 def page():
   page_state = me.state(PageState)
   form_state = me.state(FormState)
+  tweet_state = me.state(TweetState)
+  popup_state = me.state(PopupState)
   #with me.box(style=me.Style(margin=me.Margin.all(10),display="flex", flex_direction="row", justify_content="center", width="100vw", align_items="center",)):
   with me.box(style=me.Style(display="flex", flex_direction="row", width="100%")):
  
     # sidenav toggle
     with me.sidenav(opened=page_state.sidenav_open, style=me.Style(width=SIDENAV_WIDTH)):
-      me.text("Creditizens AI App")
+      with me.box(style=me.Style(display="flex", flex_direction="column", background="white", justify_content="start", align_content="center", align_items="center", flex_wrap="wrap")):
+        me.text("Creditizens AI App", type="headline-5", style=me.Style(color="blue"))
       with me.box(style=me.Style(display="flex", flex_direction="column", justify_content="start", align_content="center", align_items="center")):
         me.text("Download Report", type="headline-6")
-        me.button("Internet Search Report", on_click=post_tweet) # post_tweet function to create and also create a state variable to save last chat answer so that it can be used
+        me.button("Internet Search Report")
       with me.box(style=me.Style(display="flex", flex_direction="column", justify_content="start", align_content="center", align_items="center")):
         me.text("Social Media Post", type="headline-6")
-        me.button("Post to Tweeter")
+        me.button("Post to Tweeter", on_click=post_tweet) # post_tweet function to create and also create a state variable to save last chat answer so that it can be used
       with me.box(style=me.Style(padding=me.Padding.all(10),display="flex", flex_direction="column", background="white", justify_content="start", align_content="center", align_items="center", flex_wrap="wrap")):
         me.text("Set Key and Tokens", type="headline-6"),
-        me.text("All fields are required", type="body-2", style=me.Style(color="orange")),
+        me.text("All fields are required: (Tip) delete all fields and save again if you haven't provided all fields but saved.", type="body-2", style=me.Style(color="blue")),
         me.text("Enter consumer key"),
         me.input(type="password", value="", on_input=lambda e: on_input(form_state, "CONSUMER_KEY", e), required=True),
         me.text("Enter consumer secret"),
@@ -282,25 +357,33 @@ def page():
     # Chat side
     #with me.box(style=me.Style(margin=me.Margin.all(10),display="flex", flex_direction="row", justify_content="center", align_content="stretch", width="100vw", flex_grow="1")):
     with me.box(style=me.Style(width="90vw")):
-      mel.chat(transform, title="Mesop Demo Chat", bot_user="Creditizens Helper")
+      mel.chat(transform, title="Creditizens Agent Chat Info Tweet", bot_user="Creditizens Helper")
+
+    # Popup message management
+    if popup_state.popup_visible:
+      with me.box(style=me.Style(position="fixed", top="50%", left="50%", transform="translate(-50%, -50%)", padding=me.Padding.all(20), background="white", box_shadow="0 0 10px rgba(0,0,0,0.5)", z_index=1000)):
+        me.text(str(popup_state.popup_message), style=popup_state.popup_style)
+        # me.button("Close", on_click=close_popup)
+        me.button("Close", on_click=lambda e: close_popup(popup_state, e))
 
 
 
 def transform(input: str, history: list[mel.ChatMessage]):
-  for line in random.sample(LINES, random.randint(3, len(LINES) - 1)):
+  tweet_state = me.state(TweetState)
+  # add here the llm agents- call function logic for chat
+  message = random.sample(LINES, random.randint(3, len(LINES) - 1))
+  tweet_state.tweet_message = " ".join(message)
+  for line in message:
     time.sleep(0.3)
-
+    
     yield line + " "
 
 
 LINES = [
-  "Mesop is a Python-based UI framework designed to simplify web UI development for engineers without frontend experience.",
-  "It leverages the power of the Angular web framework and Angular Material components, allowing rapid construction of web demos and internal tools.",
-  "With Mesop, developers can enjoy a fast build-edit-refresh loop thanks to its hot reload feature, making UI tweaks and component integration seamless.",
-  "Deployment is straightforward, utilizing standard HTTP technologies.",
-  "Mesop's component library aims for comprehensive Angular Material component coverage, enhancing UI flexibility and composability.",
-  "It supports custom components for specific use cases, ensuring developers can extend its capabilities to fit their unique requirements.",
-  "Mesop's roadmap includes expanding its component library and simplifying the onboarding processs.",
+  "Shibuya is in Tokyo and is a very famous crossing.",
+  "Manga kissa are places where we can read mangas and surf in the internet while enjoying free drinks.",
+  "JR line is a line going around Tokyo and very useful as it is stopping at all main stations and helps to easily commute.",
+  "Buberry shop in Ginza is where you will find the bests sells people of Japan!.",
 ]
 
 
